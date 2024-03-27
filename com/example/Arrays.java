80fd4fc6f839eb5f73a09bec66eaabb546796383
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-03-27 10:18:47 -0400

    Initial commit

---
+++ com/example/Arrays.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Christopher Di Bert
+ * @version 1.0
+ * @since 2024-03-27
+ */
+
+// Arrays class
+public final class Arrays {
+
+    /** Private constructor to prevent instantiation. */
+    private Arrays() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
